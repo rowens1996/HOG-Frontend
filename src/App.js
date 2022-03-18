ce6b800d70b56d487be2858cbbc43d78c0072876
Ryan <96872747+rowens1996@users.noreply.github.com>
2022-03-18 11:30:27 +0000

    !!Changed the Case For Role!!

--- src/App.js
+++ src/App.js
@@ -52,11 +52,11 @@ function App() {
   const client = new ApiClient(token, logout, role, username);
 
   const roleDash = () => {
-    if (role === "student") {
+    if (role === "Student") {
       return (
         <StudentDashboard username={username} client={client} logOut={logout} />
       );
-    } else if (role === "employer") {
+    } else if (role === "Employer") {
       return <EmployerDash client={client} logOut={logout} />;
     } else if (role === "TDA") {
       return <TdaDash client={client} logOut={logout} />;
